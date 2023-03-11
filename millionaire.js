0c26d61732f417a62a24172071b26f0b35195d2d
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-03-11 00:07:43 -0500

    tweaks in proj 1

--- millionaire.js
+++ millionaire.js
@@ -1,5 +1,5 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const playButton = document.getElementById('play-btn')
+const continueButton = document.getElementById('continue-btn')
 const questionContainerElement = document.getElementById('question-container')
 const QuestionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -7,14 +7,14 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
+playButton.addEventListener('click', playGame)
+continueButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
 
-function startGame() {
-  startButton.classList.add('hide')
+function playGame() {
+  playButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - 0.5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
@@ -42,7 +42,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
-  nextButton.classList.add('hide')
+  continueButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
@@ -56,10 +56,10 @@ function selectAnswer(e) {
     setStatusClass(button, button.dataset.correct)
   })
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    continueButton.classList.remove('hide')
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    playButton.innerText = 'Restart'
+    playButton.classList.remove('hide')
   }
 }
 
@@ -77,19 +77,19 @@ function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
-
+// *an array of 10 questions including some of my favorite fun facts*
 const questions = [
   {
     question: 'Is Jon Snow a bastard?',
     answers: [
-      { text: 'Yes, he is the bastard of Ned Stark', correct: false },
+      { text: '• A: Yes, he is the bastard of Ned Stark', correct: false },
       {
-        text: 'No, he is son of King Robert Baratheon and true heir to the Iron Throne',
+        text: '• B: No, he is son of King Robert Baratheon and true heir to the Iron Throne',
         correct: false
       },
-      { text: 'No, he is son of Rhaegar Targaryen', correct: true },
+      { text: '• C: No, he is son of Rhaegar Targaryen', correct: true },
       {
-        text: 'Yes, his mother was a Wildling and his father was an unhonest Man of the Nightswatch',
+        text: '• D: Yes, his mother was a Wildling and his father was an unhonest Man of the Nightswatch',
         correct: false
       }
     ]
@@ -98,10 +98,10 @@ const questions = [
     question:
       'Which of the following is not a value in the fibonacci sequence?',
     answers: [
-      { text: '610', correct: false },
-      { text: '145', correct: true },
-      { text: '0', correct: false },
-      { text: '13', correct: false }
+      { text: '• A: 610', correct: false },
+      { text: '• B: 145', correct: true },
+      { text: '• C: 0', correct: false },
+      { text: '• D: 13', correct: false }
     ]
   },
   {
@@ -109,50 +109,53 @@ const questions = [
       'Which of the following statements about the periodic table of elements is false?',
     answers: [
       {
-        text: 'The sodium atom contains 16 electrons and 16 protons',
+        text: '• A: The sodium atom contains 16 electrons and 16 protons',
         correct: true
       },
-      { text: 'Fl, Cl, Br, and I are classified as Halogens', correct: false },
       {
-        text: 'Hydrogen is the first element in the periodic table',
+        text: '• B: Fl, Cl, Br, and I are classified as Halogens',
         correct: false
       },
-      { text: 'Na has a mass of 22.99', correct: false }
+      {
+        text: '• C: Hydrogen is the first element in the periodic table',
+        correct: false
+      },
+      { text: '• D: Na has a mass of 22.99', correct: false }
     ]
   },
   {
     question: 'What is the sq rt of 144?',
     answers: [
-      { text: '12', correct: true },
-      { text: '14', correct: false },
-      { text: '144', correct: false },
-      { text: '1', correct: false }
+      { text: '• B: 14', correct: false },
+      { text: '• C: 144', correct: false },
+      { text: '• A: 12', correct: true },
+      { text: '• D: 1', correct: false }
     ]
   },
   {
     question: 'How many bones are in the human body?',
     answers: [
-      { text: '216', correct: false },
-      { text: '192', correct: false },
-      { text: '221', correct: false },
-      { text: '206', correct: true }
+      { text: '• A: 216', correct: false },
+      { text: '• B: 192', correct: false },
+      { text: '• C: 221', correct: false },
+      { text: '• D: 206', correct: true }
     ]
   },
   {
     question:
       'What is the following line after this lyric: How can you see into my eyes like open doors?...',
     answers: [
-      { text: 'call my name and save me from the dark', correct: false },
+      { text: '• A: call my name and save me from the dark', correct: false },
       {
-        text: 'Now that I know what Im without, you cant just leave me',
+        text: '• B: Now that I know what Im without, you cant just leave me',
         correct: false
       },
       {
-        text: 'Leading you down into my core where Ive become so numb',
+        text: '• C: Leading you down into my core where Ive become so numb',
         correct: true
       },
       {
-        text: 'wake me up inside',
+        text: '• D: wake me up inside',
         correct: false
       }
     ]
@@ -161,46 +164,46 @@ const questions = [
     question: 'What does the word ergo mean?',
     answers: [
       {
-        text: 'therefore',
+        text: '• A: therefore',
         correct: true
       },
       {
-        text: 'escargo',
+        text: '• B: escargot',
         correct: false
       },
       {
-        text: 'hearing aid',
+        text: '• C: a hearing aid',
         correct: false
       },
-      { text: 'to move forward', correct: false }
+      { text: '• D: a salty fish', correct: false }
     ]
   },
   {
     question:
       'Which of the following shows did NOT air on Cartoon Network in the 90s?',
     answers: [
-      { text: 'Dragon Ball Z', correct: true },
-      { text: 'Powerpuff Girls', correct: false },
-      { text: 'Johnny Bravo', correct: false },
-      { text: 'Ed, Edd n Eddy', correct: false }
+      { text: '• B: Powerpuff Girls', correct: false },
+      { text: '• C: Johnny Bravo', correct: false },
+      { text: '• A: Dragon Ball Z', correct: true },
+      { text: '• D: Ed, Edd n Eddy', correct: false }
     ]
   },
   {
     question: 'Mauritius is...',
     answers: [
-      { text: 'the name of a famous king in the 1400s', correct: false },
-      { text: 'a tarty desert served in Arab countries', correct: false },
-      { text: 'a country in Asia', correct: false },
-      { text: 'an island off the coast of Madagascar', correct: true }
+      { text: '• A: the name of a famous king in the 1400s', correct: false },
+      { text: '• B: a tarty desert served in Arab countries', correct: false },
+      { text: '• C: a country in Asia', correct: false },
+      { text: '• D: an island off the coast of Madagascar', correct: true }
     ]
   },
   {
     question: 'By what age do babies generally start cooing?',
     answers: [
-      { text: '2 months', correct: true },
-      { text: '7 weeks', correct: false },
-      { text: '3 months', correct: false },
-      { text: '3 weeks', correct: false }
+      { text: '• B: 7 weeks', correct: false },
+      { text: '• A: 2 months', correct: true },
+      { text: '• C: 3 months', correct: false },
+      { text: '• D: 3 weeks', correct: false }
     ]
   }
 ]
